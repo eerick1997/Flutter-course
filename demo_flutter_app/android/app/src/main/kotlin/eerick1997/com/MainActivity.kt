267171a9f03747d7afe23f34f87975a868f54e85
Erick Efraín Vargas Romero <vargas.erick030997@gmail.com>
2020-06-17 18:39:33 -0500

    Hello world in flutter

---
+++ demo_flutter_app/android/app/src/main/kotlin/eerick1997/com/MainActivity.kt
@@ -0,0 +1,6 @@
+package eerick1997.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/android/app/src/main/kotlin/eerick1997/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package eerick1997.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
